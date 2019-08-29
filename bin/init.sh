2d40984572d9750e4d3d56af0188e15cd08738cd
Aditya Sidharta <aditya.sdrt@gmail.com>
2019-08-29 15:58:54 +0800

    Remove bug on init script, remove obsolete python version, add todo

--- bin/init.sh
+++ bin/init.sh
@@ -5,5 +5,5 @@ git remote add origin git@github.com:AdityaSidharta/logsensei.git
 pyenv install 3.7.2
 pyenv local 3.7.2
 pip install pipenv
-pipenv install --python 3.7.2 --dev
-flit init
+pipenv sync --python 3.7.2 --dev
+pipenv run flit init
